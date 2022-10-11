4a273fcda8223b222c00a2662f6cfa71e6d338ad
Nipun Kalansuriya <nipun.kalansuriya@arimaclanka.com>
2022-10-12 00:40:15 +0400

    feat: label component

---
+++ example/src/screens/showcase/LabelShowcase.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import { MLabelVariations, MLabel } from 'matrix';
+
+export default function LabelShowcase() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ marginVertical: 20 }}>l1</Text>
+        <MLabel content="Label" type={MLabelVariations.l1} />
+        <Text style={{ marginVertical: 20 }}>l2</Text>
+        <MLabel content="Label" type={MLabelVariations.l2} />
+        <Text style={{ marginVertical: 20 }}>l3</Text>
+        <MLabel content="Label" type={MLabelVariations.l3} />
+        <Text style={{ marginVertical: 20 }}>l4</Text>
+        <MLabel content="Label" type={MLabelVariations.l4} />
+        <Text style={{ marginVertical: 20 }}>l5</Text>
+        <MLabel content="Label" type={MLabelVariations.l5} />
+      </View>
+    </SafeAreaView>
+  );
+}
